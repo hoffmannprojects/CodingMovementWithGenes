07ac90d22301518c7c0e7ab16a5069c3f4a2262f
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-05-29 15:26:27 +0200

    Added asset package and created Chromosome class.

---
+++ Assets/_Project/Characters/Chromosome.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chromosome {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
