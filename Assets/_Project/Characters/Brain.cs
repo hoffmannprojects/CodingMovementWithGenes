4cead00d99b40207ca42ad4b814cdb643d2ec917
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-05-30 15:44:52 +0200

    L10: PopulationManager working

--- Assets/_Project/Characters/Brain.cs
+++ Assets/_Project/Characters/Brain.cs
@@ -6,14 +6,36 @@ using System.Collections;
 
 public class Brain : MonoBehaviour
 {
+    #region Properties
+    public Dna Dna { get; private set; }
+    public float TimeAlive { get; private set; }
+    #endregion
+
+    #region Fields
     private int DnaLength = 1;
-    private float timeAlive;
-    private Dna dna;
 
     private ThirdPersonCharacter thirdPersonCharacter;
     private Vector3 moveDirection;
     private bool isJumping;
     private bool isAlive = true;
+    #endregion
+
+    #region public methods
+    public void Init ()
+    {
+        // Initialize DNA
+        // 0 forward
+        // 1 back
+        // 2 left
+        // 3 right
+        // 4 jump
+        // 5 crouch
+        Dna = new Dna(DnaLength, 6);
+        thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
+        TimeAlive = 0;
+        isAlive = true;
+    }
+    #endregion
 
     // Use this for initialization
     void Start ()
@@ -29,21 +51,6 @@ public class Brain : MonoBehaviour
         }
     }
 
-    private void Init ()
-    {
-        // Initialize DNA
-        // 0 forward
-        // 1 back
-        // 2 left
-        // 3 right
-        // 4 jump
-        // 5 crouch
-        dna = new Dna(DnaLength, 6);
-        thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
-        timeAlive = 0;
-        isAlive = true;
-    }
-
     // FixedUpdate is called in sync with physics.
     private void FixedUpdate ()
     {
@@ -52,27 +59,27 @@ public class Brain : MonoBehaviour
         float vertical = 0f;
         bool isCrouching = false;
 
-        if (dna.GetGene(0) == 0)
+        if (Dna.GetGene(0) == 0)
         {
             vertical = 1;
         }
-        else if (dna.GetGene(0) == 1)
+        else if (Dna.GetGene(0) == 1)
         {
             vertical = -1;
         }
-        else if (dna.GetGene(0) == 2)
+        else if (Dna.GetGene(0) == 2)
         {
             horizontal = -1;
         }
-        else if (dna.GetGene(0) == 3)
+        else if (Dna.GetGene(0) == 3)
         {
             horizontal = 1;
         }
-        else if (dna.GetGene(0) == 4)
+        else if (Dna.GetGene(0) == 4)
         {
             isJumping = true;
         }
-        else if (dna.GetGene(0) == 5)
+        else if (Dna.GetGene(0) == 5)
         {
             isCrouching = true;
         }
@@ -83,7 +90,7 @@ public class Brain : MonoBehaviour
 
         if (isAlive)
         {
-            timeAlive += Time.deltaTime;
+            TimeAlive += Time.deltaTime;
         }
     }
 }
---
+++ Assets/_Project/Managers/PopulationManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PopulationManager : MonoBehaviour {
+
+    #region Fields
+    [SerializeField] GameObject botPrefab;
+    [SerializeField] int populationSize = 50;
+    [SerializeField] float trialTime = 5;
+
+    float timeElapsed = 0;
+    int generation = 1;
+
+    List<GameObject> population = new List<GameObject>();
+
+    GUIStyle guiStyle = new GUIStyle();
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        InitializePopulation();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeElapsed += Time.deltaTime;
+
+        if(timeElapsed >= trialTime)
+        {
+            BreedNewPopulation();
+            timeElapsed = 0;
+        }
+	}
+
+    private void OnGUI ()
+    {
+        guiStyle.fontSize = 25;
+        guiStyle.normal.textColor = Color.white;
+        GUI.BeginGroup(new Rect(10, 10, 250, 150));
+        GUI.Box(new Rect(0, 0, 140, 140), "Stats", guiStyle);
+        GUI.Label(new Rect(10, 25, 200, 30), "Gen: " + generation, guiStyle);
+        GUI.Label(new Rect(10, 50, 200, 30), string.Format("Time: {0:0.00}", timeElapsed), guiStyle);
+        GUI.Label(new Rect(10, 75, 200, 30), "Population: " + population.Count, guiStyle);
+        GUI.EndGroup();
+    }
+
+    private void InitializePopulation ()
+    {
+        for(var i = 0; i < populationSize; i++)
+        {
+            Vector3 startingPosition = new Vector3(this.transform.position.x + Random.Range(-2, 2), this.transform.position.y, this.transform.position.z + Random.Range(-2, 2));
+
+            GameObject bot = Instantiate(botPrefab, startingPosition, Quaternion.identity);
+            bot.GetComponent<Brain>().Init();
+            population.Add(bot);
+        }
+    }
+
+    private GameObject Breed(GameObject parent1, GameObject parent2)
+    {
+        Vector3 startingPosition = new Vector3(this.transform.position.x + Random.Range(-2, 2), this.transform.position.y, this.transform.position.z + Random.Range(-2, 2));
+
+        GameObject offspring = Instantiate(botPrefab, startingPosition, Quaternion.identity);
+        Brain offspringBrain = offspring.GetComponent<Brain>();
+
+        if(Random.Range(0, 100) == 1)
+        // Mutate 1 of 100.
+        {
+            offspringBrain.Init();
+            offspringBrain.Dna.Mutate();
+        }
+        else
+        // Combine parent Dna.
+        {
+            offspringBrain.Init();
+            offspringBrain.Dna.Combine(parent1.GetComponent<Brain>().Dna, parent2.GetComponent<Brain>().Dna);
+        }
+        return offspring;
+    }
+
+    private void BreedNewPopulation()
+    {
+        List<GameObject> sortedPopulation = population.OrderBy(g => g.GetComponent<Brain>().TimeAlive).ToList();
+        population.Clear();
+
+        // Breed second half of sortedPopulation.
+        // TODO: What happens for uneven population counts (float vs int)?
+        for(var i = (sortedPopulation.Count / 2) - 1; i < sortedPopulation.Count - 1; i++)
+        {
+            // Breed two offsprings from a pair of parents:
+            // One with parent1's first half and parent2's second half of DNA
+            // and one the other way around.
+            population.Add(Breed(sortedPopulation[i], sortedPopulation[i + 1]));
+            population.Add(Breed(sortedPopulation[i + 1], sortedPopulation[i]));
+        }
+        // Destroy all parents and previous population.
+        for(var i = 0; i < sortedPopulation.Count; i++)
+        {
+            Destroy(sortedPopulation[i]);
+        }
+        generation++;
+    }
+}
